dfc0166ec110a75295d87a474123bf6841287a7b
Stefan Walther <swr@qlik.com>
2017-11-25 02:26:22 +0100

    style: satisfy linter

--- src/lib/AmqpSugar.js
+++ src/lib/AmqpSugar.js
@@ -1,4 +1,5 @@
 const logger = require('winster').instance();
+// eslint-disable-next-line capitalized-comments
 // const promiseRetry = require('promise-retry');
 
 class AmqpSugar {
@@ -6,7 +7,6 @@ class AmqpSugar {
   constructor() {
     this.logger = logger;
   }
-
 
 }
 
