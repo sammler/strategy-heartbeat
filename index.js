36c6b06cd4cf9f899756d59e42bebaa9eeaca82f
Stefan Walther <swr@qlik.com>
2017-12-01 21:50:08 -0700

    feat: handle termination signals

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const AppServer = require('./src/server.js');
+const ON_DEATH = require('death');
 
 let server = new AppServer();
+
+ON_DEATH(async (signal, err) => {
+  console.log('');
+  console.log('on-death');
+  await server.stop();
+});
+
 server.start();
 
-process.on('SIGTERM', function () {
-  console.log('sigterm');
-  server.stop();
-
-  // Wait for all SIGTERM listeners to gracefully stop before forcing process exit
-  process.nextTick(function () {
-    process.exit()
-  })
-});
