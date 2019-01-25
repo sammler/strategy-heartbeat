d4d686c3de6eab76da8ef3b08781c90d41274423
Stefan Walther <swr@qlik.com>
2019-01-25 21:45:16 +0100

    refactor: using winster

--- src/config/routes-config.js
+++ src/config/routes-config.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
 const glob = require('glob');
-const debug = require('debug')('strategy-heartbeat:routes-config');
+const logger = require('winster').instance();
 
 const router = express.Router(); // eslint-disable-line new-cap
 
 // Load routes based on the pattern './../modules/**/*.routes.js
 let routes = glob.sync(path.join(__dirname, './../modules/**/*.routes.js'));
-debug('------ Routes');
+logger.trace('------ ++ Routes');
 routes.forEach(r => {
-  debug('Registering route', r);
+  logger.trace('Registering route', r);
   router.use('/', require(r));
 });
-debug('------- /Routes');
+logger.trace('------ // Routes');
 
 module.exports = router;
